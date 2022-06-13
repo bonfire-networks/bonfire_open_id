d654c47d69a6b416a3d9807be0a78d862a06b4c8
Mayel de Borniol <git@mayel.space>
2022-06-14 09:04:58 +1200

    Use compile_env

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -26,7 +26,7 @@ defmodule Bonfire.OpenID.ChannelCase do
       import Bonfire.OpenID.ChannelCase
 
       # The default endpoint for testing
-      @endpoint Bonfire.Common.Config.get!(:endpoint_module)
+      @endpoint Application.compile_env!(:bonfire, :endpoint_module)
     end
   end
 
