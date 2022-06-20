45ca76e082a679f8287a5f7cf3fbca788b8d5b44
Mayel de Borniol <git@mayel.space>
2022-06-20 15:51:04 +1200

    Excluding tags

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-ExUnit.start()
+ExUnit.start(exclude: Bonfire.Common.RuntimeConfig.skip_test_tags())
 Ecto.Adapters.SQL.Sandbox.mode(Bonfire.Common.Config.get!(:repo_module), :manual)
 
 # to test openid/oauth
