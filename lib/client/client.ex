901c1828498dfb0c97bce145e67cf86581708320
Mayel de Borniol <git@mayel.space>
2024-08-31 19:01:08 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1016

--- lib/client/client.ex
+++ lib/client/client.ex
@@ -6,23 +6,26 @@ defmodule Bonfire.OpenID.Client do
 
   def oauth2_providers, do: Application.get_env(:bonfire_open_id, :oauth2_providers, [])
 
-  def providers_authorization_urls do
-    for {provider, config} <- open_id_connect_providers() do
-      {
-        config[:display_name] || provider,
-        Utils.ok_unwrap(
-          OpenIDConnect.authorization_uri(Map.new(config), %{
-            "state" => Bonfire.Common.Text.random_string(),
-            "nonce" => Bonfire.Common.Text.random_string()
-          })
-        )
-      }
-    end ++
-      for {provider, config} <- oauth2_providers() do
-        {
-          config[:display_name] || provider,
-          config[:redirect_uri]
-        }
-      end
+  def providers_authorization_urls(source \\ :login) do
+    (for {provider, config} <- open_id_connect_providers() do
+       unless source == :signup and config[:only_supports_login],
+         do: {
+           config[:display_name] || provider,
+           Utils.ok_unwrap(
+             OpenIDConnect.authorization_uri(Map.new(config), %{
+               "state" => Bonfire.Common.Text.random_string(),
+               "nonce" => Bonfire.Common.Text.random_string()
+             })
+           )
+         }
+     end ++
+       for {provider, config} <- oauth2_providers() do
+         unless source == :signup and config[:only_supports_login],
+           do: {
+             config[:display_name] || provider,
+             config[:redirect_uri]
+           }
+       end)
+    |> Enum.reject(&is_nil/1)
   end
 end
--- lib/web/components/sign_in_with_external_providers_live.ex
+++ lib/web/components/sign_in_with_external_providers_live.ex
@@ -2,4 +2,5 @@ defmodule Bonfire.OpenID.Web.SignInWithExternalProvidersLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop label, :any, default: nil
+  prop source, :atom, default: nil
 end
