1956b6fadb8765fdbc6e1c9c23546e44f8d3ba54
Mayel de Borniol <git@mayel.space>
2025-08-26 16:22:02 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1491

--- lib/web/components/sign_in_with_external_providers_live.ex
+++ lib/web/components/sign_in_with_external_providers_live.ex
@@ -3,4 +3,12 @@ defmodule Bonfire.OpenID.Web.SignInWithExternalProvidersLive do
 
   prop label, :any, default: nil
   prop source, :atom, default: nil
+  prop include_providers, :list, default: nil
+
+  def filter_providers(providers, nil), do: providers
+  def filter_providers(providers, []), do: providers
+
+  def filter_providers(providers, include_providers) when is_list(include_providers) do
+    Enum.filter(providers, fn {provider, _} -> provider in include_providers end)
+  end
 end
