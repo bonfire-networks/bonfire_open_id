ce1b58440847f01b7b15fffb38f9b1d80a8a3e0c
Mayel de Borniol <git@mayel.space>
2026-08-26 12:57:23 +0100

    optimise

--- priv/repo/migrations/20251218162107_clients_key_pair_types.exs
+++ priv/repo/migrations/20251218162107_clients_key_pair_types.exs
@@ -2,4 +2,7 @@ defmodule Bonfire.Common.Repo.Migrations.ClientsKeyPairTypes do
   use Ecto.Migration
 
   use Boruta.Migrations.ClientsKeyPairTypes
+
+  # the macro's `change/0` runs an `execute "UPDATE oauth_clients …"`, which Ecto can't invert. We don't need to reverse boruta's schema, just to not block rolling back ours
+  def down, do: nil
 end
--- priv/repo/migrations/20251218162109_clients_signatures_adapters.exs
+++ priv/repo/migrations/20251218162109_clients_signatures_adapters.exs
@@ -2,4 +2,7 @@ defmodule Bonfire.Common.Repo.Migrations.ClientsSignaturesAdapters do
   use Ecto.Migration
 
   use Boruta.Migrations.ClientsSignaturesAdapters
+
+  # the macro's `change/0` does `modify :did, :text` with no `from:`, which Ecto can't invert. We don't need to reverse boruta's schema, just to not block rolling back ours
+  def down, do: nil
 end
